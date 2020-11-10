f3e6de83b0e3b8b95a0807d6b36592d3ac6eda63
Nikitas Filosofof <nikitas4717@gmail.com>
2020-11-10 10:16:34 +0100

    fucken mess

--- script.js
+++ script.js
@@ -8,10 +8,32 @@ function computerPlay() {
 let playerSelection
 let playerSelectionOG
 
+let button = Array.from(document.querySelectorAll('.option'));
+let buttonid = document.querySelector(`.option[id="${e.getAttribute('id')}"]`);
+button.forEach()
+
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+button[0].addEventListener('click', () => {
+    alert("Hello World");
+  });
+//button.getAttribute('id');
+
+  button.addEventListener('click', function (e) {
+    console.log(e.getAttribute('id'));
+  });
+
 function userSelect() {
+    
+
+
     playerSelectionOG = prompt(`What are you gonna play?`)
     playerSelection = playerSelectionOG.toLowerCase()
 }
+
+
 
 
 function checkResults (playerSelection, computerSelection) {
@@ -27,11 +49,10 @@ function checkResults (playerSelection, computerSelection) {
 
 
 function game() {
-    for (i = 1 ; i <=5 ; i++){
     computerPlay()
     userSelect()
     console.log(checkResults(playerSelection,computerSelection))
-    }
+    
 }
 
 game()
